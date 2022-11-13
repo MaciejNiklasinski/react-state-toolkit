57ae8f68ba53fa64bd449b0436459c1f2b239983
Maciej Niklasinski <maciej.niklasinski@gmail.com>
2022-11-13 23:39:15 +0000

    Refactored to minimize number of instantiated validators

--- src/factories/actions.js
+++ src/factories/actions.js
@@ -17,151 +17,144 @@ export const getActionsFactory = ({
   actionsImports,
   selectors,
   selectorsImports,
-}) => ({
-  createAction: ({
-    storeName = DEFAULT_STORE,
-    sliceName,
-    name,
-    func,
-  }) => {
-    const suffixedName = suffixIfRequired(name, "Action");
-    const { validateAction } = getActionValidator({
-      stores,
-      slices,
-      actions,
-      actionsByType,
-      actionsImports,
-      selectors,
-      selectorsImports,
-    });
-    validateAction({ storeName, sliceName, actionName: suffixedName, func });
-
-    if (!stores[storeName]) stores[storeName] = {};
-    if (!stores[storeName].actions) stores[storeName].actions = {};
-    if (!stores[storeName].actions[sliceName])
-      stores[storeName].actions[sliceName] = {};
-
-    const type = Symbol(name);
-    const action = params => {
-      const { getState, getActions, getSelectors, status } = stores[storeName];
-      if (status === STATUS.REDUCING)
-        throw new UnableToInvokeReducingStoreAction({ actionId });
-      else if (status === STATUS.SELECTING)
-        throw new UnableToInvokeSelectingStoreAction({ actionId });
-      const payload = func(params, { getState, getActions, getSelectors });
-      const result = { sliceName, params, type, payload };
-      stores[storeName].dispatch(result);
-      return result;
-    };
-
-    stores[storeName].actions[sliceName][suffixedName] = action;
-    const actionId = getActionId({
-      storeName,
+}) => {
+  const { validateAction } = getActionValidator({
+    stores,
+    slices,
+    actions,
+    actionsByType,
+    actionsImports,
+    selectors,
+    selectorsImports,
+  });
+  return {
+    createAction: ({
+      storeName = DEFAULT_STORE,
       sliceName,
-      actionName: suffixedName,
-    });
-    actions[actionId] = Object.freeze({
-      storeName,
-      sliceName,
-      actionName: suffixedName,
-      actionType: type,
-      [suffixedName]: action,
-      [toScreamingSnakeCase(suffixedName)]: type,
-    });
-    actionsByType[type] = actions[actionId];
-    const safeAction = (...params) => {
-      if (!stores[storeName]?.initialized)
-        throw new UnableToInvokeUninitializedStoreAction({ actionId });
-      return action(...params);
-    };
-    return {
-      ...actions[actionId],
-      [suffixedName]: safeAction,
-    };
-  },
+      name,
+      func,
+    }) => {
+      const suffixedName = suffixIfRequired(name, "Action");
+      validateAction({ storeName, sliceName, actionName: suffixedName, func });
 
-  createAsyncAction: ({
-    storeName = DEFAULT_STORE,
-    sliceName,
-    name,
-    func,
-    rethrow = true,
-  }) => {
-    const suffixedName = suffixIfRequired(name, "Action");
-    const { validateAction } = getActionValidator({
-      stores,
-      slices,
-      actions,
-      actionsByType,
-      actionsImports,
-      selectors,
-      selectorsImports,
-    });
-    validateAction({ storeName, sliceName, actionName: suffixedName, func });
+      if (!stores[storeName]) stores[storeName] = {};
+      if (!stores[storeName].actions) stores[storeName].actions = {};
+      if (!stores[storeName].actions[sliceName])
+        stores[storeName].actions[sliceName] = {};
 
-    if (!stores[storeName]) stores[storeName] = {};
-    if (!stores[storeName].actions) stores[storeName].actions = {};
-    if (!stores[storeName].actions[sliceName])
-      stores[storeName].actions[sliceName] = {};
+      const type = Symbol(name);
+      const action = params => {
+        const { getState, getActions, getSelectors, status } = stores[storeName];
+        if (status === STATUS.REDUCING)
+          throw new UnableToInvokeReducingStoreAction({ actionId });
+        else if (status === STATUS.SELECTING)
+          throw new UnableToInvokeSelectingStoreAction({ actionId });
+        const payload = func(params, { getState, getActions, getSelectors });
+        const result = { sliceName, params, type, payload };
+        stores[storeName].dispatch(result);
+        return result;
+      };
 
-    const PENDING = Symbol(`${name}.${TYPE_SUFFIXES.PENDING}`);
-    const REJECTED = Symbol(`${name}.${TYPE_SUFFIXES.REJECTED}`);
-    const RESOLVED = Symbol(`${name}.${TYPE_SUFFIXES.RESOLVED}`);
-    const type = { PENDING, REJECTED, RESOLVED };
-
-    const action = async params => {
-      const { getState, getActions, getSelectors, status } = stores[storeName];
-      if (status === STATUS.REDUCING)
-        throw new UnableToInvokeReducingStoreAction({ actionId });
-      else if (status === STATUS.SELECTING)
-        throw new UnableToInvokeSelectingStoreAction({ actionId });
-      stores[storeName].dispatch({ sliceName, params, type: PENDING });
-      return new Promise(async (resolve, reject) => {
-        let result;
-        try {
-          const payload = await func(params, {
-            getState,
-            getActions,
-            getSelectors,
-          });
-          result = { sliceName, params, type: RESOLVED, payload };
-        } catch (error) {
-          result = { sliceName, params, type: REJECTED, error, rethrow };
-        }
-
-        try { stores[storeName].dispatch(result); }
-        catch (error) { reject(error); }
-
-        if (result.error && result.rethrow)
-          reject(result.error)
-        else resolve(result);
+      stores[storeName].actions[sliceName][suffixedName] = action;
+      const actionId = getActionId({
+        storeName,
+        sliceName,
+        actionName: suffixedName,
       });
-    };
-    stores[storeName].actions[sliceName][suffixedName] = action;
-    const actionId = getActionId({
-      storeName,
+      actions[actionId] = Object.freeze({
+        storeName,
+        sliceName,
+        actionName: suffixedName,
+        actionType: type,
+        [suffixedName]: action,
+        [toScreamingSnakeCase(suffixedName)]: type,
+      });
+      actionsByType[type] = actions[actionId];
+      const safeAction = (...params) => {
+        if (!stores[storeName]?.initialized)
+          throw new UnableToInvokeUninitializedStoreAction({ actionId });
+        return action(...params);
+      };
+      return {
+        ...actions[actionId],
+        [suffixedName]: safeAction,
+      };
+    },
+
+    createAsyncAction: ({
+      storeName = DEFAULT_STORE,
       sliceName,
-      actionName: suffixedName,
-    });
-    actions[actionId] = Object.freeze({
-      storeName,
-      sliceName,
-      actionName: suffixedName,
-      actionType: type,
-      [suffixedName]: action,
-      [toScreamingSnakeCase(suffixedName)]: type,
-    });
-    actionsByType[PENDING] = actions[actionId];
-    actionsByType[REJECTED] = actions[actionId];
-    actionsByType[RESOLVED] = actions[actionId];
-    const safeAction = (...params) => {
-      if (!stores[storeName]?.initialized)
-        throw new UnableToInvokeUninitializedStoreAction({ actionId });
-      return action(...params);
-    };
-    return {
-      ...actions[actionId],
-      [suffixedName]: safeAction,
-    };
-  },
-});
+      name,
+      func,
+      rethrow = true,
+    }) => {
+      const suffixedName = suffixIfRequired(name, "Action");
+      validateAction({ storeName, sliceName, actionName: suffixedName, func });
+
+      if (!stores[storeName]) stores[storeName] = {};
+      if (!stores[storeName].actions) stores[storeName].actions = {};
+      if (!stores[storeName].actions[sliceName])
+        stores[storeName].actions[sliceName] = {};
+
+      const PENDING = Symbol(`${name}.${TYPE_SUFFIXES.PENDING}`);
+      const REJECTED = Symbol(`${name}.${TYPE_SUFFIXES.REJECTED}`);
+      const RESOLVED = Symbol(`${name}.${TYPE_SUFFIXES.RESOLVED}`);
+      const type = { PENDING, REJECTED, RESOLVED };
+
+      const action = async params => {
+        const { getState, getActions, getSelectors, status } = stores[storeName];
+        if (status === STATUS.REDUCING)
+          throw new UnableToInvokeReducingStoreAction({ actionId });
+        else if (status === STATUS.SELECTING)
+          throw new UnableToInvokeSelectingStoreAction({ actionId });
+        stores[storeName].dispatch({ sliceName, params, type: PENDING });
+        return new Promise(async (resolve, reject) => {
+          let result;
+          try {
+            const payload = await func(params, {
+              getState,
+              getActions,
+              getSelectors,
+            });
+            result = { sliceName, params, type: RESOLVED, payload };
+          } catch (error) {
+            result = { sliceName, params, type: REJECTED, error, rethrow };
+          }
+
+          try { stores[storeName].dispatch(result); }
+          catch (error) { reject(error); }
+
+          if (result.error && result.rethrow)
+            reject(result.error)
+          else resolve(result);
+        });
+      };
+      stores[storeName].actions[sliceName][suffixedName] = action;
+      const actionId = getActionId({
+        storeName,
+        sliceName,
+        actionName: suffixedName,
+      });
+      actions[actionId] = Object.freeze({
+        storeName,
+        sliceName,
+        actionName: suffixedName,
+        actionType: type,
+        [suffixedName]: action,
+        [toScreamingSnakeCase(suffixedName)]: type,
+      });
+      actionsByType[PENDING] = actions[actionId];
+      actionsByType[REJECTED] = actions[actionId];
+      actionsByType[RESOLVED] = actions[actionId];
+      const safeAction = (...params) => {
+        if (!stores[storeName]?.initialized)
+          throw new UnableToInvokeUninitializedStoreAction({ actionId });
+        return action(...params);
+      };
+      return {
+        ...actions[actionId],
+        [suffixedName]: safeAction,
+      };
+    },
+  };
+};
--- src/factories/stores.js
+++ src/factories/stores.js
@@ -12,154 +12,157 @@ export const getStoresFactory = ({
   actionsImports,
   selectors,
   selectorsImports,
-}) => ({
-  createStore: ({
-    name = DEFAULT_STORE,
-    storeSlices = {},
-    storeSelectors = {},
-    isStrictDevMode = false,
-  }) => {
-    // Normalize store creations arguments
-    if (storeSlices && typeof storeSlices === "object")
-      storeSlices = Object.values(storeSlices);
-    if (storeSelectors && typeof storeSelectors === "object")
-      storeSelectors = Object.values(storeSelectors);
+}) => {
+  return {
+    createStore: ({
+      name = DEFAULT_STORE,
+      storeSlices = {},
+      storeSelectors = {},
+      isStrictDevMode = false,
+    }) => {
+      // Normalize store creations arguments
+      if (storeSlices && typeof storeSlices === "object")
+        storeSlices = Object.values(storeSlices);
+      if (storeSelectors && typeof storeSelectors === "object")
+        storeSelectors = Object.values(storeSelectors);
 
-    // Create validators
-    const { validateStore } = getStoreValidator({
-      stores,
-      slices,
-      actions,
-      actionsByType,
-      actionsImports,
-      selectors,
-      selectorsImports,
-    });
-    // Validate store creation
-    validateStore({ storeName: name, storeSlices, storeSelectors });
+      // Create validators
+      const { validateStore } = getStoreValidator({
+        stores,
+        slices,
+        actions,
+        actionsByType,
+        actionsImports,
+        selectors,
+        selectorsImports,
+      });
+      // Validate store creation
+      validateStore({ storeName: name, storeSlices, storeSelectors });
 
-    const {
-      getUseStoreState,
-      getUseSelector,
-      getUseSelectorMemo
-    } = getHooksFactory({
-      stores,
-      slices,
-      actions,
-      actionsByType,
-      actionsImports,
-      selectors,
-      selectorsImports,
-    });
+      const {
+        getUseStoreState,
+        getUseSelector,
+        getUseSelectorMemo
+      } = getHooksFactory({
+        stores,
+        slices,
+        actions,
+        actionsByType,
+        actionsImports,
+        selectors,
+        selectorsImports,
+      });
 
-    // Store state subscriptions
-    const triggersStack = new Map();
-    const subscriptionsMatrix = new Map();
+      // Store state subscriptions
+      const triggersStack = new Map();
+      const subscriptionsMatrix = new Map();
 
-    // Create store functions
-    const dispatch = action => {
-      const reducer = stores[name].reducers[action.sliceName][action.type];
-      if (!reducer || action instanceof Promise) return;
+      // Create store functions
+      const dispatch = action => {
+        const reducer = stores[name].reducers[action.sliceName][action.type];
+        if (!reducer || action instanceof Promise) return;
 
-      stores[name].status = STATUS.REDUCING;
-      const newSliceState = { ...stores[name].state[action.sliceName] };
-      try { reducer(newSliceState, action); }
-      catch (error) {
+        stores[name].status = STATUS.REDUCING;
+        const newSliceState = { ...stores[name].state[action.sliceName] };
+        try { reducer(newSliceState, action); }
+        catch (error) {
+          stores[name].status = STATUS.READY;
+          throw error;
+        }
+        const newState = Object.freeze({
+          ...stores[name].state,
+          [action.sliceName]: Object.freeze(newSliceState),
+        });
+        stores[name].state = newState;
+        stores[name].stateVersion = Symbol();
+
+        stores[name].status = STATUS.SELECTING;
+        subscriptionsMatrix.forEach(({ onStateChange }) => onStateChange(newState));
+
+        stores[name].status = STATUS.RENDERING;
+        triggersStack.forEach(renderTrigger => {
+          const { requiresRender, value, setSelected } = renderTrigger;
+          if (!requiresRender) return;
+          renderTrigger.requiresRender = false;
+          renderTrigger.value = null;
+          setSelected(value);
+        });
         stores[name].status = STATUS.READY;
-        throw error;
-      }
-      const newState = Object.freeze({
-        ...stores[name].state,
-        [action.sliceName]: Object.freeze(newSliceState),
-      });
-      stores[name].state = newState;
-      stores[name].stateVersion = Symbol();
-
-      stores[name].status = STATUS.SELECTING;
-      subscriptionsMatrix.forEach(({ onStateChange }) => onStateChange(newState));
-
-      stores[name].status = STATUS.RENDERING;
-      triggersStack.forEach(renderTrigger => {
-        const { requiresRender, value, setSelected } = renderTrigger;
-        if (!requiresRender) return;
-        renderTrigger.requiresRender = false;
-        renderTrigger.value = null;
-        setSelected(value);
-      });
-      stores[name].status = STATUS.READY;
-    };
-
-    const useStoreState = getUseStoreState({ storeName: name, isStrictDevMode });
-    const useSelector = getUseSelector({ storeName: name, isStrictDevMode });
-    const useSelectorMemo = getUseSelectorMemo({ storeName: name, isStrictDevMode });
-
-    const getState = (sliceName = null) =>
-      sliceName ? stores[name].state[sliceName] : stores[name].state;
-    const getActions = (sliceName = null) =>
-      sliceName ? stores[name].actions[sliceName] : stores[name].actions;
-    const getSelectors = (sliceName = null) =>
-      sliceName ? stores[name].selectors[sliceName] : stores[name].selectors;
-    const getHooks = () => ({ useStoreState, useSelector, useSelectorMemo });
-    const withStore = Component => props =>
-      createElement(Component, { ...{ ...props, getActions, getSelectors, getHooks } }, null);
-
-    // Assign validated store imports
-    const storeActionImports = actionsImports[name] || {};
-    Object.keys(storeActionImports).forEach(
-      (sliceName) => Object.keys(storeActionImports[sliceName]).forEach(
-        (actionName) => storeActionImports[sliceName][actionName].forEach(assignImport => assignImport())
-      )
-    );
-    const storeSelectorImports = selectorsImports[name] || {};
-    Object.keys(storeSelectorImports).forEach(
-      (sliceName) => Object.keys(storeSelectorImports[sliceName]).forEach(
-        (selectorName) => storeSelectorImports[sliceName][selectorName].forEach(assignImport => assignImport())
-      )
-    );
-
-    // Create and freeze store
-    stores[name].triggersStack = triggersStack;
-    stores[name].subscriptionsMatrix = subscriptionsMatrix;
-    stores[name].dispatch = dispatch;
-    stores[name].useStoreState = useStoreState;
-    stores[name].useSelector = useSelector;
-    stores[name].getState = getState;
-    stores[name].getActions = getActions;
-    stores[name].getSelectors = getSelectors;
-    stores[name].getHooks = getHooks;
-    stores[name].name = name;
-    stores[name].stateVersion = Symbol();
-    stores[name].initialized = true;
-
-    Object.freeze(stores[name].reducers);
-    Object.freeze(stores[name].selectors);
-    Object.keys(stores[name].actions).forEach(sliceName =>
-      Object.freeze(stores[name].actions[sliceName]),
-    );
-    Object.freeze(stores[name].actions);
-
-    let storeExport = {
-      withStore,
-      useStoreState,
-      useSelector,
-      useSelectorMemo,
-      getState,
-      getActions,
-      getSelectors,
-      getHooks,
-    };
-    if (name !== DEFAULT_STORE)
-      storeExport = {
-        ...storeExport,
-        [insertCapitalized('withStore', 4, name)]: withStore,
-        [insertCapitalized('useStoreState', 3, name)]: useStoreState,
-        [insertCapitalized('useSelector', 3, name)]: useSelector,
-        [insertCapitalized('useSelectorMemo', 3, name)]: useSelectorMemo,
-        [insertCapitalized('getState', 3, name)]: getState,
-        [insertCapitalized('getActions', 3, name)]: getActions,
-        [insertCapitalized('getSelectors', 3, name)]: getSelectors,
-        [insertCapitalized('getHooks', 3, name)]: getHooks,
       };
-    return Object.freeze(storeExport);
-  },
-});
+
+      const useStoreState = getUseStoreState({ storeName: name, isStrictDevMode });
+      const useSelector = getUseSelector({ storeName: name, isStrictDevMode });
+      const useSelectorMemo = getUseSelectorMemo({ storeName: name, isStrictDevMode });
+
+      const getState = (sliceName = null) =>
+        sliceName ? stores[name].state[sliceName] : stores[name].state;
+      const getActions = (sliceName = null) =>
+        sliceName ? stores[name].actions[sliceName] : stores[name].actions;
+      const getSelectors = (sliceName = null) =>
+        sliceName ? stores[name].selectors[sliceName] : stores[name].selectors;
+      const getHooks = () => ({ useStoreState, useSelector, useSelectorMemo });
+      const withStore = Component => props =>
+        createElement(Component, { ...{ ...props, getActions, getSelectors, getHooks } }, null);
+
+      // Assign validated store imports
+      const storeActionImports = actionsImports[name] || {};
+      Object.keys(storeActionImports).forEach(
+        (sliceName) => Object.keys(storeActionImports[sliceName]).forEach(
+          (actionName) => storeActionImports[sliceName][actionName].forEach(assignImport => assignImport())
+        )
+      );
+      const storeSelectorImports = selectorsImports[name] || {};
+      Object.keys(storeSelectorImports).forEach(
+        (sliceName) => Object.keys(storeSelectorImports[sliceName]).forEach(
+          (selectorName) => storeSelectorImports[sliceName][selectorName].forEach(assignImport => assignImport())
+        )
+      );
+
+      // Create and freeze store
+      stores[name].triggersStack = triggersStack;
+      stores[name].subscriptionsMatrix = subscriptionsMatrix;
+      stores[name].dispatch = dispatch;
+      stores[name].useStoreState = useStoreState;
+      stores[name].useSelector = useSelector;
+      stores[name].getState = getState;
+      stores[name].getActions = getActions;
+      stores[name].getSelectors = getSelectors;
+      stores[name].getHooks = getHooks;
+      stores[name].name = name;
+      stores[name].stateVersion = Symbol();
+      stores[name].status = STATUS.READY;
+      stores[name].initialized = true;
+
+      Object.freeze(stores[name].reducers);
+      Object.freeze(stores[name].selectors);
+      Object.keys(stores[name].actions).forEach(sliceName =>
+        Object.freeze(stores[name].actions[sliceName]),
+      );
+      Object.freeze(stores[name].actions);
+
+      let storeExport = {
+        withStore,
+        useStoreState,
+        useSelector,
+        useSelectorMemo,
+        getState,
+        getActions,
+        getSelectors,
+        getHooks,
+      };
+      if (name !== DEFAULT_STORE)
+        storeExport = {
+          ...storeExport,
+          [insertCapitalized('withStore', 4, name)]: withStore,
+          [insertCapitalized('useStoreState', 3, name)]: useStoreState,
+          [insertCapitalized('useSelector', 3, name)]: useSelector,
+          [insertCapitalized('useSelectorMemo', 3, name)]: useSelectorMemo,
+          [insertCapitalized('getState', 3, name)]: getState,
+          [insertCapitalized('getActions', 3, name)]: getActions,
+          [insertCapitalized('getSelectors', 3, name)]: getSelectors,
+          [insertCapitalized('getHooks', 3, name)]: getHooks,
+        };
+      return Object.freeze(storeExport);
+    },
+  };
+};
